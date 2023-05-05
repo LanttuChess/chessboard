6c214b35638c3fd96f3d304082f4ae6b9a15a1a7
LanttuChess <antti.halava@gmail.com>
2023-05-05 09:14:33 +0300

    Update app.py to use POST request

--- app.py
+++ app.py
@@ -1,8 +1,20 @@
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello_world():
-    return jsonify({"message": "Welcome to my REST API!"})
+def hello():
+    return "Hello World!"
+
+
+@app.route("/api", methods=["POST"])
+def reverse_string():
+    data = request.get_json()
+
+    if 'input_string' not in data:
+        return jsonify({"message": "Missing input_string"}), 400
+    
+    reverse_string = data['input_string'][::-1]
+
+    return jsonify({"reverse_string": reverse_string})
