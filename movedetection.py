da9ce4bdf7bc0ccd5ee6de964ba640c7524145c7
LanttuChess <antti.halava@gmail.com>
2023-05-15 13:48:34 +0300

    Update movedetection.py

--- movedetection.py
+++ movedetection.py
@@ -1,10 +1,12 @@
 from enum import Enum, auto
 
+import chess
+import chesslogic
+
 class PhysicalState(Enum):
     NO_PIECE_PICKED = auto()
     ONE_PIECE_PICKED = auto()
     TWO_PIECES_PICKED = auto()
-    PIECE_ADDED = auto()
 
 class ChangeState(Enum):
     NO_CHANGE = auto()
@@ -21,13 +23,14 @@ def format_occupancy_data(input_string):
 
 
 current_physical_state = PhysicalState.NO_PIECE_PICKED
-current_occupancy_data = format_occupancy_data("1,1,1,1,1,1,1,1/1,1,1,1,1,1,1,1/0,0,0,0,0,0,0,0/0,0,0,0,0,0,0,0/0,0,0,0,0,0,0,0/0,0,0,0,0,0,0,0/0,1,1,1,1,1,1,1/1,1,1,1,1,1,1,1")
+current_picked_piece = (None, None, (None, None)) # (piece_type, color, location)
+current_occupancy_data = format_occupancy_data("1,1,1,1,1,1,1,1/1,1,1,1,1,1,1,1/0,0,0,0,0,0,0,0/0,0,0,0,0,0,0,0/0,0,0,0,0,0,0,0/0,0,0,0,0,0,0,0/1,1,1,1,1,1,1,1/1,1,1,1,1,1,1,1")
 
 
 
 def reset_physical_state():
     current_physical_state = PhysicalState.NO_PIECE_PICKED
-    current_occupancy_data = format_occupancy_data("1,1,1,1,1,1,1,1/1,1,1,1,1,1,1,1/0,0,0,0,0,0,0,0/0,0,0,0,0,0,0,0/0,0,0,0,0,0,0,0/0,0,0,0,0,0,0,0/0,1,1,1,1,1,1,1/1,1,1,1,1,1,1,1")
+    current_occupancy_data = format_occupancy_data("1,1,1,1,1,1,1,1/1,1,1,1,1,1,1,1/0,0,0,0,0,0,0,0/0,0,0,0,0,0,0,0/0,0,0,0,0,0,0,0/0,0,0,0,0,0,0,0/1,1,1,1,1,1,1,1/1,1,1,1,1,1,1,1")
 
 
 
@@ -37,6 +40,48 @@ def state_change_detector(input_string):
     comparision, location = compare_occupancy_data(current_occupancy_data, new_occupancy_data)
 
     print(f"comparision: {comparision}, location: {location}")
+
+
+    if comparision == ChangeState.NO_CHANGE:
+        print("")
+        # do nothing
+    elif comparision == ChangeState.ADDED_PIECE:
+        print("Added piece at: ", location)
+        # complete move
+
+        if current_physical_state == PhysicalState.ONE_PIECE_PICKED:
+            # complete move
+            print("complete move")
+        elif current_physical_state == PhysicalState.TWO_PIECES_PICKED:
+            # complete move
+            print("complete move, with two pieces picked")
+        else:
+            # error
+            print("error")
+
+
+    elif comparision == ChangeState.REMOVED_PIECE:
+        print("Removed piece at: ", location)
+        # either begin move, or secondary effect
+
+        if current_physical_state == PhysicalState.NO_PIECE_PICKED:
+            # begin move
+            print("begin move")
+
+            piece = chesslogic.board.piece_at(chess.square(*location))
+            print(piece)
+
+        elif current_physical_state == PhysicalState.ONE_PIECE_PICKED:
+            # either capture or castling
+            print("capture or castling")
+        else:
+            # error
+            print("error")
+
+    elif comparision == ChangeState.ILLEGAL:
+        print("Illegal")
+        # do nothing, return error
+
 
     return "Pass"
 
