a5622208f3be71efaeb17a9d04da02e111b63b8d
LanttuChess <antti.halava@gmail.com>
2023-05-22 06:15:46 +0300

    Sum change implementation

--- movedetection/MoveDetection.py
+++ movedetection/MoveDetection.py
@@ -4,6 +4,7 @@ class MoveDetection:
         self.matrix = [[0 for _ in range(size)] for _ in range(size)]
         self.previous_matrix = None
         self.change_sum = 0
+        self.minus_position = None
         if initial_state:
             self.parse_input(initial_state)
 
@@ -20,17 +21,24 @@ class MoveDetection:
             return None
 
         changes = [[0 for _ in range(self.size)] for _ in range(self.size)]
+        special_cases = []
         for i in range(self.size):
             for j in range(self.size):
                 diff = self.matrix[i][j] - self.previous_matrix[i][j]
                 if diff > 0:
                     changes[i][j] = 1
-                    self.change_sum += 1
+                    if self.change_sum < 0:
+                        special_cases.append((self.minus_position, (i, j)))
+                        self.change_sum = 0
+                    else:
+                        self.change_sum += 1
                 elif diff < 0:
                     changes[i][j] = -1
                     self.change_sum -= 1
+                    if self.change_sum == -1:
+                        self.minus_position = (i, j)
 
-        return changes
+        return changes, special_cases
 
     def get_current_matrix(self):
         return self.matrix
