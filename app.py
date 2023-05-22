4e03013b7b5e127f106da905249543568eec25aa
LanttuChess <antti.halava@gmail.com>
2023-05-22 19:54:54 +0300

    Fix Hex code issue

--- app.py
+++ app.py
@@ -100,5 +100,5 @@ def pieces_with_legal_moves():
 def board_with_legal_moves():
     board = chesslogic.get_board_with_legal_moves(chesslogic.board)
     rgb_data = board.replace('0', '000000')
-    rgb_data = board.replace('1', '0000FF')
+    rgb_data = rgb_data.replace('1', '0000FF')
     return jsonify({'message': "Fetched board with legal moves successfully.", 'data': board, 'rgb': rgb_data}), 200
