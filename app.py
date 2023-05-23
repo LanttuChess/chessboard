e4490311e94a4fc700d2d2d4a99627d01b16a4a1
LanttuChess <antti.halava@gmail.com>
2023-05-23 13:27:29 +0300

    data -> matrix_data

--- app.py
+++ app.py
@@ -134,5 +134,5 @@ def board_with_legal_moves():
     board = chesslogic.get_board_with_legal_moves(chesslogic.board)
     rgb_data = board.replace('0', '000000')
     rgb_data = rgb_data.replace('1', '0000FF').replace(",", "").replace("/", "")
-    return jsonify({'message': "Fetched board with legal moves successfully.", 'data': board, 'rgb_data': rgb_data}), 200
+    return jsonify({'message': "Fetched board with legal moves successfully.", 'matrix_data': board, 'rgb_data': rgb_data}), 200
 
